df0c0fa4202d181f64d8d3b3cac1dd55b1c2fcff
TMD <albmagerit@gmail.com>
2023-01-01 19:37:18 +0100

    recomment

--- inputgen.py
+++ inputgen.py
@@ -1,10 +1,9 @@
 import cupy as np
 
 def generate_inputs(batch_size):
-    # Generate random integers between 1 and 999_999_999
+    # Generate random inputs
     inputs = np.random.randint(1, 999_999_999, size=(batch_size, batch_size))
     # Normalize the inputs
     inputs_norm = inputs - np.min(inputs)
     inputs_norm = inputs / (np.max(inputs) - np.min(inputs))
-    # Return the normalized inputs
     return inputs_norm
